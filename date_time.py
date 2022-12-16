b69020314bbef16a3af49fd35889c0bcc193a957
geek-nish <114519822+geek-nish@users.noreply.github.com>
2022-12-16 19:49:38 +0530

    Delete date_time.py

--- date_time.py
+++
@@ -1,8 +0,0 @@
-#For Date and Time
-from datetime import datetime
-
-now = datetime.now()
-today_date=datetime.date(datetime.now()) #Current Date
-current_time = now.strftime("%H:%M:%S") #Current Time
-
-
